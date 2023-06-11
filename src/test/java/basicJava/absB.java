cca25d60906deab31f4d18b79fac004779f113e2
Arvind Kumar <arvind@DESKTOP-9T6KQTB>
2023-06-11 14:51:03 +0530

    try1

--- src/test/java/basicJava/absB.java
+++ src/test/java/basicJava/absB.java
@@ -16,5 +16,20 @@ public abstract class absB implements interfaceA{
 	{
 		System.out.println("I am C");
 	}
+	
+	public void d()
+	{
+		System.out.println("I am D");
+	}
+	
+	public void e()
+	{
+		System.out.println("I am E");
+	}
+	
+	public void f()
+	{
+		System.out.println("I am F");
+	}
 
 }
