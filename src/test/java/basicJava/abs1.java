a158294fd70b78c980a45dfe8b4e9e1e2a173ba2
Arvind Kumar <arvind@DESKTOP-9T6KQTB>
2023-06-11 11:38:07 +0530

    abs1

--- src/test/java/basicJava/abs1.java
+++ src/test/java/basicJava/abs1.java
@@ -3,7 +3,7 @@ package basicJava;
 public class abs1 {
 
 	public abs1() {
-		// TODO Auto-generated constructor stub
+		System.out.println("I am a");
 	}
 
 	public static void main(String[] args) {
